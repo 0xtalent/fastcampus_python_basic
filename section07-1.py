b85aa195bc85cd33f2a5dbde8332d8f712e9deb9
marinehayong <marinehayong@gmail.com>
2019-08-23 17:30:18 +0900

    Self, class variable, instance variable 2 finish

--- section07-1.py
+++ section07-1.py
@@ -2,6 +2,11 @@
 # 파이썬 클래스 상세 이해
 # Self, 클래스, 인스턴스 변수
 
+# 클래스, 인스턴스 차이 중요
+# 네임스페이스 : 객체를 인스턴스화 할 때 저장된 공강
+# 클래스 변수 : 직접 사용 가능, 객체보다 먼저 생성
+# 인스턴스 변수 : 객체마다 별도로 존재, 인스턴스 생성 후 사용
+
 # 선언
 # class 클래스명:
 #     함수
@@ -80,3 +85,75 @@ print(user2.__dict__)
 # 생략합니다. 영상보세요
 # 오 어제처럼 이해감!! 감사합니다 윤인성님!!
 # section07-1 강의는 다시 한번 공부하기
+
+# 예제2
+# self의 이해
+class SelfTest():
+    @staticmethod
+    def function1():
+        print('function1 called!')
+# 이거 왜 오류 뜨지...실행은 되는데...
+# 짜증나서 구글검색
+# https://hashcode.co.kr/questions/1051/클래스-메소드에-self를-적고-안적고는-무슨-차이인가요
+# self를 붙인 쪽을 bound, 안 붙인 쪽은 unbound 메소드라 합니다.
+# self가 받아주는 역할로 있어야 하는데 self가 없기 때문에 인자가 너무 많이 들어왔다는 겁니다.
+# self를 쓰기 싫으면 데코레이트터를 써서 이 메소드는 bound method를 만들지 마라고 설정할 수 있습니다.
+# 무슨 말인지는 잘 모르겠지만, 대충 무슨 느낌인지 알겠어서, 데코레이터를 서서 해결
+
+    def function2(self):
+        print(id(self))
+        print('function2 called!')
+
+
+self_test = SelfTest()
+# self_test.function1()
+SelfTest.function1()
+self_test.function2()
+
+print(id(self_test))
+# self_test = 인자가 위에 자동으로 넘어간겨
+# 너무 복잡하게 생각하지 말고 인스턴스를 생성하는 메소드는 셀프인자가 자동으로 넘어간다
+# 셀프가 없는 것들은 클래스에서 직접 호출한다
+# 깊은 내용을 하고 있으니 어렵게 느껴질 수 있으니, 공부 하는 과정 속에서 자동적으로 이해가 갈 것임
+# 이해 하고 넘어가도 괜찮습니다
+
+# 예제3
+# 클래스 변수, 인스턴스 변수
+
+
+class WareHouse:
+    # 클래스 변수
+    stock_num = 0
+
+    def __init__(self, name):
+        self.name = name
+        WareHouse.stock_num += 1
+
+    def __del__(self):
+        WareHouse.stock_num += 1
+
+
+user1 = WareHouse('Jung')
+user2 = WareHouse('Park')
+user3 = WareHouse('Lee')
+
+print(user1.__dict__)
+print(user2.__dict__)
+print(user3.__dict__)
+print(WareHouse.__dict__)  # 클래스 네임스페이스, 클래스 변수(공유)
+
+print(user1.name)
+print(user2.name)
+print(user3.name)
+# 흑흑 잘 모르겠어요...클래스 변수...
+# 나중에 다시 공부할게요...유튜브로도 공부할게요...
+# 더 진행하면서 이해되니까 스트레스 받지 말라고 하시네요...ㅎㅎ
+
+print(user1.stock_num)
+print(user2.stock_num)
+print(user3.stock_num)
+
+del user1
+
+print(user2.stock_num)
+print(user3.stock_num)
